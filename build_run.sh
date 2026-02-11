13df14f22b316ba804c4d2adb01c3c3e4d742d0c
bekzat <bkbdrsh@gmail.com>
2026-02-11 14:34:33 +0500

    cpu -> core

--- build_run.sh
+++ build_run.sh
@@ -39,7 +39,7 @@ cd "$RTL_ROOT"
 verilator --trace -cc \
   -Wall \
   -I"$RTL_ROOT/rlt" \
-  soc/cpu.sv \
+  soc/core.sv \
   soc/rf.sv soc/pc.sv soc/exu.sv soc/idu.sv soc/alu.sv soc/csr.sv soc/com.sv soc/icache.sv \
   --timescale "1ns/1ns" \
   --no-timing \
